fcb5b38783b0432eb8683bcb1f63e7fa92ebf6aa
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-09-28 06:11:22 -0300

    .

--- SAGA_SA_Demanda_Lunary_Sergipe_Cetafest/RestauranteLunary/Models/UserLocalStorage.py
+++ SAGA_SA_Demanda_Lunary_Sergipe_Cetafest/RestauranteLunary/Models/UserLocalStorage.py
@@ -1,2 +1,3 @@
 MemoryCard = {}
 MemoryCard['9960baf3-a5c9-4fbb-a3ea-dfce2e159fb2'] = {'Id': 1,'auth': 0, 'Cardapio': []}
+MemoryCard['9960baf3-a5c9-4fbb-a3ea-dfce2e159fb2'] = {'Id': 2,'auth': 0, 'Cardapio': []}
